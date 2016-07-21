d7426ac741affb42ebb965eb064570ade499fc1d
Owen Andrews <joandrews33@users.noreply.github.com>
2016-07-21 11:41:37 -0400

    Fixed rounding error when printing to file.

--- qSR/SpatialClustering/BioJet/BioJetTree.m
+++ qSR/SpatialClustering/BioJet/BioJetTree.m
@@ -6,13 +6,13 @@ function tree=BioJetTree(Frames,Xpos,Ypos,Intensity,writefilename,fastjetoutfile
     %Rescale Position Data. Note: One dimension in fastjet is periodic with period 2pi, so
     %restricting it to the region [0,1] will prevent forming "wraparound"
     %clusters.
-    largest_value=max([Xpos,Ypos]);
+    largest_value=max([Xpos,Ypos]); 
     XposScaled=Xpos/largest_value;
     YposScaled=Ypos/largest_value;
     
     %Write data into a text document that BioJetsTree can read
     write_data=[1:length(Frames);Frames;XposScaled;YposScaled;Intensity]';
-    dlmwrite(writefilename,write_data,' ')
+    dlmwrite(writefilename,write_data,'delimiter',' ','precision',16)
     
     %Change to the BioJetsTree directory, execute the program and return to
     %the present directory. 
