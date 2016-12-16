7dd28cadbf75cb0b1fab1f6a31482b23d4110b97
joandrews33 <joandrews33@gmail.com>
2016-12-16 13:48:09 -0500

    Fixed a number of bugs. Addressed off by 1 error in definition of dark time tolerance. Corrected mishandling of data which is not pre-sorted. Fixed implementation of dark time tolerance to be locl between points, and not characteristic of whole cluster

--- qSR/SpatioTemporalClustering/QuickMerge/QuickMerge.m
+++ qSR/SpatioTemporalClustering/QuickMerge/QuickMerge.m
@@ -1,4 +1,4 @@
-function ids=QuickMerge(times,xpos,ypos,sigma,dark_time)
+function ids_out=QuickMerge(times,xpos,ypos,sigma,dark_time)
 
     N=length(times);
     
@@ -8,29 +8,35 @@ function ids=QuickMerge(times,xpos,ypos,sigma,dark_time)
     
     ids=zeros(1,N);
     cluster_number=0;
-    cluster_end_times=[];
+    %cluster_end_times=[];
     progress_bar_handle=waitbar(0,'Running Spatiotemporal Merge');
     for i = 1:N
+        
+        %Display Progress
         if mod(i,round(N/100)) ==0
             if ishandle(progress_bar_handle)
                 waitbar(i/N,progress_bar_handle,'Running Spatiotemporal Merge')
             end
         end
+        
+        %Initialize new cluster?
         if ids(i) == 0
             cluster_number=cluster_number+1;
             ids(i)=cluster_number;
-            cluster_end_times(cluster_number)=s_times(i);
+            %cluster_end_times(cluster_number)=s_times(i);
         end
+        
+        
         for j = (i+1):N
-            if (s_times(j)-cluster_end_times(ids(i)))<=dark_time 
+            if (s_times(j)-s_times(i))-1<=dark_time 
                 dist=sqrt((s_xpos(i)-s_xpos(j))^2+(s_ypos(i)-s_ypos(j))^2);
-                if dist < 2.5*sigma
-                    if ids(j) == 0;
+                if dist < sigma
+                    if ids(j) == 0
                         ids(j)=ids(i);
-                        cluster_end_times(ids(i))=max(cluster_end_times(ids(i)),s_times(j));
+                        %cluster_end_times(ids(i))=max(cluster_end_times(ids(i)),s_times(j));
                     else
                         ids(ids==ids(j))=ids(i);
-                        cluster_end_times(ids(i))=max(cluster_end_times(ids(i)),cluster_end_times(ids(j)));
+                        %cluster_end_times(ids(i))=max(cluster_end_times(ids(i)),cluster_end_times(ids(j)));
                     end
                     
                 end
@@ -40,6 +46,15 @@ function ids=QuickMerge(times,xpos,ypos,sigma,dark_time)
             
         end
     end
+    
+    unique_ids=unique(ids);
+    ids_out=zeros(size(ids));
+    for i = 1:length(unique_ids)
+        ids_out(ids==unique_ids(i))=i;
+    end
+    
+    [~,unsort_idxs]=sort(sort_idxs);
+    ids_out=ids_out(unsort_idxs);
     
     if ishandle(progress_bar_handle)
         close(progress_bar_handle)
